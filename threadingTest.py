acdc7945d5fd6356a72a03a7d7b183da559c9240
nfantinodyer <n_fantinodyer@u.pacific.edu>
2023-06-13 16:05:50 -0700

    thread without class works

--- threadingTest.py
+++ threadingTest.py
@@ -3,48 +3,46 @@ import tkinter as tk
 import time
 import os
 
-class Application:
-    def __init__(self, master):
-        self.master = master
-        self.pause_event = threading.Event()
-        self.pause_event.set()  # Start in un-paused state
-        self.thread = threading.Thread(target=self.begin)
-        
-        self.entry = tk.Entry(master)
-        self.entry.pack()
-        
-        self.start_button = tk.Button(master, text="Start", command=self.start)
-        self.start_button.pack()
-        
-        self.pause_button = tk.Button(master, text="Pause", command=self.pause)
-        self.pause_button.pack()
-        
-        self.resume_button = tk.Button(master, text="Resume", command=self.resume)
-        self.resume_button.pack()
-        
-        self.exit_button = tk.Button(master, text="Exit", command=self.exit)
-        self.exit_button.pack()
+# Event for pausing/unpausing the thread
+pause_event = threading.Event()
+pause_event.set()  # Start in un-paused state
 
-    def start(self):
-        date = self.entry.get()
-        self.thread.start()
+# Define your functions
+def start():
+    thread.start()
 
-    def pause(self):
-        self.pause_event.clear()  # Pause the thread
+def pause():
+    pause_event.clear()  # Pause the thread
 
-    def resume(self):
-        self.pause_event.set()  # Resume the thread
+def resume():
+    pause_event.set()  # Resume the thread
 
-    def exit(self):
-        os._exit(0)
+def exit():
+    os._exit(0)
 
-    def begin(self):
-        # Use self.pause_event.wait() instead of time.sleep()
-        while True:
-            print("Working...")
-            while not self.pause_event.is_set():
-                time.sleep(0.1)
+def begin():
+    # Use self.pause_event.wait() instead of time.sleep()
+    while True:
+        print("Working...")
+        while not pause_event.is_set():
+            time.sleep(0.1)
+
+# Create your thread
+thread = threading.Thread(target=begin)
 
 root = tk.Tk()
-app = Application(root)
+
+# Create your buttons
+start_button = tk.Button(text="Start", command=start)
+start_button.pack()
+
+pause_button = tk.Button(text="Pause", command=pause)
+pause_button.pack()
+
+resume_button = tk.Button(text="Resume", command=resume)
+resume_button.pack()
+
+exit_button = tk.Button(text="Exit", command=exit)
+exit_button.pack()
+
 root.mainloop()
