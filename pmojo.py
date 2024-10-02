082e7ec34495d74259669b8f2808c19d909f616b
Nicholas Fantino-Dyer <2013RECK@gmail.com>
2024-10-02 13:57:50 -0700

    Updated code in full method for better handling
    Doubled up names now shows both appt times separated by an &.

--- pmojo.py
+++ pmojo.py
@@ -4,21 +4,15 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from pywinauto.application import Application
 from pywinauto.keyboard import send_keys
-import os
-import os.path
 import tkinter as tk
 import re
 from ahk import AHK
 import time
 import json
-import threading
 import warnings
 
 #to ignore pywinauto 32bit warning
 warnings.simplefilter('ignore', category=UserWarning)
-
-#I should make a class in order to initialize the threads and to
-#better organize my code
 
 #auto hot key
 ahk = AHK()
@@ -280,98 +274,45 @@ def full(cdi,cdn,d,m,y):
     go.write(clip_text)
     go.close()
 
-    #read lines of the file
-    text_file = open('practicemojo.txt', 'r')
-    text = text_file.readlines()
-    text_file.close()
+    with open('practicemojo.txt', 'r', encoding='utf-8') as text_file:
+        text = text_file.readlines()
 
-    #remove extra parts on the page
-    del text[0:7+1]
+    # Remove header and footer lines if not part of the needed data
+    del text[0:8]
     del text[-5:]
 
-    #logic to get names dates and times from file
-    new_file = open("practicemojo.txt", "w+")
-    temp = text
-    skip5=0
-    next6=0
-    skip=False
-    alltext=""
-    single=[]
-    liststring=[]
-    lastlength = 1
-    totallines = 0
+    appointments = {}
 
-    for line in temp:
-        totallines += 1
-        #if line[0:lastlength+3] in single:
-        #    if re.search("@", line):
-                #puts first time first no matter the order in practice mojo. Removes second occurance.
-        #        for li in liststring:
-        #            if li[0:lastlength+3]==line[0:lastlength+3]:
-                        #since 12pm is earlier than 3pm but 12>3 so this corrects that and fixes 7am time just in case
-        #               if line[next6+1:next6+3]<="12" and line[next6+1:next6+3]>="7":
-        #                    if line[next6+1:next6+3] > li[next6+1:next6+3]:
-        #                        single = single[0:len(single)-(len(li)-6)]
-        #                        alltext = alltext[0:len(alltext)-(len(li)-6)]
-                        #since 3pm is before 4pm and 3<4 
-        #                elif line[next6+1:next6+3] < li[next6+1:next6+3]:
-        #                    single = single[0:len(single)-(len(li)-6)]
-        #                    alltext = alltext[0:len(alltext)-(len(li)-6)]    
+    for line in text:
+        # Normalize spaces and remove non-ASCII characters
+        line = re.sub(r"\s+", ' ', line.strip())
+        line = re.sub(r"[^\x00-\x7F]+", '', line)
 
-        #skips family and gets time and date after the @ sign.
-        count=0
-        for letter in line:
-            count=count+1
-            if letter == ",":
-                lastlength = count
+        # Match the expected line format with dates and times
+        match = re.search(r"([a-zA-Z, ]+)(\d{2}/\d{2} @ \d{2}:\d{2} [APM]+)", line)
+        if match:
+            name, datetime = match.groups()
+            date, time = datetime.split(' @ ')
+            # Construct a unique key for each person and date
+            key = f"{name.strip()}{date}"
+            if key not in appointments:
+                appointments[key] = []
+            appointments[key].append(time)
+        else:
+            print(f"No match for line: {line}")  # Debug output for unmatched lines
 
-        for i in single:
-            if re.fullmatch(line[0:lastlength+3], i[0:lastlength+3]):
-                skip=True
-                break
-        
-        if not skip:
-            single.append(line)
-            liststring.append(line)
-            if re.search("Family", line):
-                continue
-            if re.search("Address",line):
-                lengthRemove = 0
-                for i in range(5, len(line)):
-                    if line[i] == "A" and line[i+1] == "d" and line[i+2] == "d" and line[i+3] == "r" and line[i+4] == "e" and line[i+5] == "s":
-                        lengthRemove = i
-                        break
-                line = line[0:lengthRemove]
-                line += "\n"
-            if re.search("@", line):
-                count=0
-                for letter in line:
-                    count=count+1
-                    if skip5>0:
-                        skip5 -= 1
-                    elif letter == " " and count==1:
-                        skip5 = 5
-                    elif letter == ",":
-                        lastlength = count
-                        alltext += letter
-                    elif letter == "@":
-                        alltext += "@"
-                        next6=count
-                    else:
-                        alltext += letter
-        skip=False
+    # Prepare to write consolidated entries to file
+    lines_to_write = []
+    for key, times in appointments.items():
+        # Combine multiple times into one line
+        times_str = ' & '.join(sorted(set(times)))  # Remove duplicates and sort times
+        name, date = key.rsplit('10', 1)
+        line_to_write = f"{name}10{date} @ {times_str}"
+        lines_to_write.append(line_to_write)
 
-
-        alltext = alltext.rstrip()
-        alltext+="\n"
-        
-    #dont need to replace opt out and address supressed due to the next6 being the next 6 chars 00:00AM
-    alltext = alltext[:-1]
-    alltext = alltext.replace("\t","")
-    alltext = alltext.strip()
-
-    for line in alltext:
-        new_file.write(line)
+    # Write all lines to file, avoiding an extra newline on the last line
+    with open("practicemojo.txt", "w+", encoding='utf-8') as new_file:
+        new_file.write("\n".join(lines_to_write))
 
     new_file.close()
 
