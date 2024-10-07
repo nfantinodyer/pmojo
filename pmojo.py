7eec5a69e73171f152fac78c653bf0c1bd76ef8a
Nicholas Fantino-Dyer <2013RECK@gmail.com>
2024-10-06 19:26:59 -0700

    Implemented threading, pausing, quit, and reorganized code

--- pmojo.py
+++ pmojo.py
@@ -7,35 +7,76 @@ from pywinauto.keyboard import send_keys
 import tkinter as tk
 import re
 from ahk import AHK
-import time
+from time import sleep
 import json
 import warnings
+import threading
+import keyboard
 
-#to ignore pywinauto 32bit warning
-warnings.simplefilter('ignore', category=UserWarning)
+warnings.simplefilter('ignore', category=UserWarning)  # Ignore 32bit warnings
 
-#auto hot key
 ahk = AHK()
+pauseEvent = threading.Event()
+pauseEvent.set()
+root = tk.Tk()
+root.withdraw()
 
-#to have a gui to type in date
-window = tk.Tk()
-window.title("Pmojo")
-window.columnconfigure(0, weight=1,minsize=250)
-window.rowconfigure(0, weight=1, minsize=250)
+def stopProgram():
+    print("Stop key pressed. Exiting program...")
+    root.quit()
 
-#starts program
+def get_clipboard():
+    return root.clipboard_get()
+
+def createGUI():
+    window = tk.Toplevel(root)
+    window.title("Pmojo")
+    window.columnconfigure(0, weight=1, minsize=250)
+    window.rowconfigure(0, weight=1, minsize=250)
+
+    entrylabel = tk.Label(window, text="Enter the date MM/DD/YYYY: ")
+    entrylabel.pack(side=tk.TOP, anchor=tk.W)
+
+    frame1 = tk.Frame(window, width=50, relief=tk.SUNKEN)
+    frame1.pack(fill=tk.X)
+
+    entry = tk.Entry(frame1, width=50)
+    entry.pack(side=tk.LEFT, padx=2, pady=5)
+
+    start_button = tk.Button(window, text="Start", width=10, height=2, command=lambda: begin(entry.get()))
+    start_button.pack(anchor=tk.W, padx=2, pady=5)
+
+    pause_button = tk.Button(window, text="Pause/Resume", width=15, height=2, command=pause)
+    pause_button.pack(anchor=tk.W, padx=2, pady=5)
+
+    exit_button = tk.Button(window, text="Exit", command=window.quit, width=10, height=2)
+    exit_button.pack(anchor=tk.W, padx=2, pady=5)
+
+    # Bind Tab key to focus the next widget
+    window.bind_all("<Tab>", lambda e: window.tk_focusNext().focus())
+    # Bind Enter key to activate the focused button
+    window.bind_all("<Return>", lambda e: window.focus_get().invoke())
+
+    window.mainloop()
+
 def begin(date):
+    threading.Thread(target=loginToSite, args=(date,)).start()
 
-    #assign month day and year
-    m = date[0:2]
-    d = date[3:5]
-    y = date[6:10]
+def pause():
+    if pauseEvent.is_set():
+        pauseEvent.clear()
+        print("Resumed")
+    else:
+        pauseEvent.set()
+        print("Paused")
 
-    #open chrome with notifs disabled and with chrome driver linked
+def focus(app):
+    app.set_focus()
+
+def loginToSite(date):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--disable-notifications")
     driver = webdriver.Chrome(options=chrome_options)
-
 
     with open('config.json') as config_file:
         config = json.load(config_file)
@@ -53,14 +94,29 @@ def begin(date):
     elem.send_keys(password)
     elem.send_keys(Keys.RETURN)
 
-    #to be able to set focus with chrome.set_focus()
+    #to be able to set focus with chrome.set_focus() and now focus(chrome)
     app = Application()
     app.connect(title_re='.*- Google Chrome')
     chrome = app.window(title_re='.*- Google Chrome')
 
-    #initial focus set
-    chrome.set_focus()
+    #to be able to set focus with softdent.set_focus() and now focus(softdent)
+    soft = Application()
+    soft.connect(title_re='.*CS SoftDent.*- S.*')
+    softdent = soft.window(title_re='.*CS SoftDent.*- S.*')
+    
+    #for testing
+    #soft.connect(title_re='.*WordPad.*')
+    #softdent = soft.window(title_re='.*WordPad.*')
 
+    #initial focus set
+    focus(chrome)
+
+    # Navigate through the site
+    processSite(driver, date, chrome, softdent)
+    driver.quit()
+
+def processSite(driver, date, chrome, softdent):
+    m, d, y = date.split('/')
     #url
     cdi = [1,8,13,21,22,23,30,33,35,36,130]
     for i in cdi:
@@ -68,102 +124,67 @@ def begin(date):
         if(i != 23 and i != 130):
             for o in range(1,3):
                 driver.get("https://app.practicemojo.com/cgi-bin/WebObjects/PracticeMojo.woa/wa/gotoActivityDetail?td="+m+"%2F"+d+"%2F"+y+"&cdi="+str(i)+"&cdn="+str(o))
-                justName(i,o,d,m,y)
+                name(i, o, d, m, y, chrome, softdent)
         else:
             for o in range(1,3):
                 driver.get("https://app.practicemojo.com/cgi-bin/WebObjects/PracticeMojo.woa/wa/gotoActivityDetail?td="+m+"%2F"+d+"%2F"+y+"&cdi="+str(i)+"&cdn="+str(o))
-                full(i,o,d,m,y)
-            
+                nameDateTime(i, o, d, m, y, chrome, softdent)
 
-def justName(cdi,cdn,d,m,y):
-    #to be able to set focus with chrome.set_focus()
-    app = Application()
-    app.connect(title_re='.*- Google Chrome')
-    chrome = app.window(title_re='.*- Google Chrome')
-
-    #to be able to set focus with softdent.set_focus()
-    soft = Application()
-    soft.connect(title_re='.*CS SoftDent.*- S')
-    softdent = soft.window(title_re='.*CS SoftDent.*- S')
-
+def name(cdi, cdn, d, m, y, chrome, softdent):
     #determine if letter email or text based on url
     typeOfCom = ""
     if cdn == 1:
-        if cdi == 1 or cdi == 8 or cdi == 21 or cdi == 22 or cdi == 30 or cdi == 33 or cdi == 35:
+        if cdi in {1, 8, 21, 22, 30, 33, 35}:
             typeOfCom = "l"
         elif cdi == 36:
             typeOfCom = "e"
     elif cdn == 2:
-        if cdi == 1 or cdi == 8 or cdi == 30 or cdi == 33:
+        if cdi in {1, 8, 30, 33}:
             typeOfCom = "e"
-    elif cdn == 3:
-        if cdi == 1:
-            typeOfCom = "t"
+    elif cdn == 3 and cdi == 1:
+        typeOfCom = "t"
 
-    #determine commuication sent based on url
-    com = ""
-    if cdi == 1:
-        com = "Recare: Due"
-    elif cdi == 8:
-        com = "Recare: Really Past Due"
-    elif cdi == 21 or cdi == 22 or cdi == 36:
-        com = "bday card"
-    elif cdi == 30:
-        com = "Reactivate: 1 year ago"
-    elif cdi == 33:
-        com = "Recare: Past Due"
-    elif cdi == 35:
-        com = "Anniversary card"
+    # determine communication sent based on url
+    com = {1: "Recare: Due", 8: "Recare: Really Past Due", 21: "bday card", 22: "bday card",
+           30: "Reactivate: 1 year ago", 33: "Recare: Past Due", 35: "Anniversary card", 36: "bday card"}.get(cdi, "")
 
-    #to copy page text
-    #time.sleep(1)
+    # to copy page text
     send_keys("^a^c")
+    clip_text = get_clipboard()
 
-    #get clipboard
-    root = tk.Tk()
-    root.withdraw()
-    clip_text = root.clipboard_get()
+    # create/open txt file to hold copied page contents and paste it in
+    with open('practicemojo.txt', "w", encoding="utf-8") as file:
+        file.write(clip_text)
 
-    #create/open txt file to hold copied page contents and paste it in
-    go = open('practicemojo.txt' ,"w",encoding="utf-8")
-    go.write(clip_text)
-    go.close()
-
-    with open('practicemojo.txt', 'r') as text_file:
-        text = text_file.readlines()
+    # read the copied text
+    with open('practicemojo.txt', 'r') as file:
+        text = file.readlines()
 
     # Remove header and footer lines
-    del text[0:8]
-    del text[-5:]
-
+    text = text[8:-5]
     unique_lines = set()
     alltext = ""
 
     for line in text:
-        # Normalize the line by removing extra spaces and unwanted tabs
-        line = re.sub(r"\s+", ' ', line.strip())
-
-        # Remove any trailing 'Address suppressed' or similar unwanted text
-        address_index = line.find("Address")
-        if address_index != -1:
-            line = line[:address_index]  # Trim the line at the word "Address"
-
-        # Remove non-ASCII characters and specific unwanted phrases
+        if "Family" in line:  # Skip the line containing "Family" as it's not needed
+            continue
         line = re.sub(r"[^\x00-\x7F]+", '', line)  # Remove non-ASCII characters
-        line = line.replace("Bounced", "")  # Remove the word "Bounced"
-        line = line.replace("Opt Out", "")  # Remove the phrase "Opt Out"
-        line = line.replace("\t", "")  # Remove all tabs
+        line = re.sub(r"\s+", ' ', line).strip()  # Normalize spaces and remove unwanted tabs
 
-        # Check for duplicates
+        if "Address" in line:
+            line = line[:line.find("Address")]  # Trim the line at the word "Address"
+
+        line = line.replace("Bounced", "").replace("Opt Out", "")
+
         if line not in unique_lines:
             unique_lines.add(line)
             alltext += line + "\n"
 
     # Write the processed lines to the same file, ensuring there's no extra newline at the end
-    with open("practicemojo.txt", "w") as new_file:
-        new_file.write(alltext.strip())
+    with open("practicemojo.txt", "w") as file:
+        file.write(alltext.strip())
         
-    new_file.close()
+    file.close()
 
     #softdent
 
@@ -172,54 +193,46 @@ def justName(cdi,cdn,d,m,y):
     file1.close()
 
     #brings softdent to the front
-    softdent.set_focus()
-
+    focus(softdent)
     #auto hot key types in softdent and program sleeps after every major input for human to double check
     for line in lines:
         if line != "":
-                
-            line = line.replace("\n","")
+            line = line.strip()
+
             ahk.key_press("Tab")
             ahk.type('f')
-                
+            pauseEvent.wait()
             ahk.type(line)
-            time.sleep(1)
+            sleep(1)
             ahk.key_press('Enter')
+            pauseEvent.wait()
             ahk.type("0ca")
             #recare is r
             if cdi == 1 or cdi == 8 or cdi == 33:
                 ahk.type("r")
             ahk.key_press("Tab")
             ahk.type(com)
+            pauseEvent.wait()
             ahk.key_press("Tab")
             ahk.key_press("Tab")
             ahk.type(typeOfCom)
             ahk.key_press("Tab")
             ahk.type("pmojoNFD")
             ahk.key_press("Tab")
+            
             ahk.type(m+"/"+d+"/"+y)
-            time.sleep(2)
-                
+            sleep(2)
+            pauseEvent.wait()
             ahk.key_press("Enter")
-
+            pauseEvent.wait()
             ahk.type("l")
             ahk.type("l")
             ahk.type("n")
 
     #chrome
-    chrome.set_focus()
+    focus(chrome)
 
-def full(cdi,cdn,d,m,y):
-    #to be able to set focus with chrome.set_focus()
-    app = Application()
-    app.connect(title_re='.*- Google Chrome')
-    chrome = app.window(title_re='.*- Google Chrome')
-
-    #to be able to set focus with softdent.set_focus()
-    soft = Application()
-    soft.connect(title_re='.*CS SoftDent.*- S')
-    softdent = soft.window(title_re='.*CS SoftDent.*- S')
-
+def nameDateTime(cdi, cdn, d, m, y, chrome, softdent):
     #determine if letter email or text based on url
     typeOfCom = ""
     if cdn == 1:
@@ -232,10 +245,7 @@ def full(cdi,cdn,d,m,y):
     #to copy page text
     send_keys("^a^c")
 
-    #get clipboard
-    root = tk.Tk()
-    root.withdraw()
-    clip_text = root.clipboard_get()
+    clip_text = get_clipboard()
 
     #create/open txt file to hold copied page contents and paste it in
     go = open('practicemojo.txt' ,"w",encoding="utf-8")
@@ -252,6 +262,8 @@ def full(cdi,cdn,d,m,y):
     appointments = {}
 
     for line in text:
+        if re.search("Family",line):  # Skip the line containing "Family" as it's not needed
+            continue
         # Normalize spaces and remove non-ASCII characters
         line = re.sub(r"\s+", ' ', line.strip())
         line = re.sub(r"[^\x00-\x7F]+", '', line)
@@ -294,8 +306,7 @@ def full(cdi,cdn,d,m,y):
     lines = file1.readlines()
     file1.close()
 
-    softdent.set_focus()
-
+    focus(softdent)
     for line in lines:
         now = False
         name = ""
@@ -310,15 +321,17 @@ def full(cdi,cdn,d,m,y):
             line = line.replace("\n","")
             ahk.key_press("Tab")
             ahk.type('f')
-                
+            pauseEvent.wait()
             ahk.type(name)
-            time.sleep(1)
+            sleep(1)
             ahk.key_press("Enter")
+            pauseEvent.wait()
             ahk.type("0ca")
             #Confirm Appt is cc
             ahk.type("cc")
             ahk.key_press("Tab")
             ahk.type("Reminder for ")
+            pauseEvent.wait()
             size = 0
             for word in line:
                 size+=1
@@ -330,13 +343,10 @@ def full(cdi,cdn,d,m,y):
                     ahk.key_press("Space")
                     ahk.type(word)
                     now = True
-                
-            for word in line:
-                if re.fullmatch("M", word):
-                    ahk.type("M")
-                    break
-                else:
-                    ahk.type(word)
+            
+            ahk.type(line)
+
+            pauseEvent.wait()
             ahk.key_press("Tab")
             ahk.key_press("Tab")
             ahk.type(typeOfCom)
@@ -344,32 +354,20 @@ def full(cdi,cdn,d,m,y):
             ahk.type("pmojoNFD")
             ahk.key_press("Tab")
             ahk.type(m+"/"+d+"/"+y)
-            time.sleep(2)
-                
+            sleep(2)
+            pauseEvent.wait()
             ahk.key_press("Enter")
-
+            pauseEvent.wait()
             ahk.type("l")
             ahk.type("l")
             ahk.type("n")
 
     #chrome
-    chrome.set_focus()
+    focus(chrome)
 
+# Register hotkeys
+keyboard.add_hotkey('num lock', stopProgram)
+keyboard.add_hotkey('scroll lock', pause)
 
-#all for gui
-entrylabel = tk.Label(text="Enter the date MM/DD/YYYY: ")
-entrylabel.pack(side=tk.TOP,anchor=tk.W)
-
-frame1 = tk.Frame(window, width=50, relief=tk.SUNKEN)
-frame1.pack(fill=tk.X)
-
-one = tk.Entry(frame1,width=50)
-one.pack(side = tk.LEFT,padx=2,pady=5)
-
-button = tk.Button(text="Start", width=10, height=2, command=lambda: begin(one.get()))
-button.pack(anchor=tk.W,padx=2, pady=5)
-
-button = tk.Button(text="Exit", command=quit, width=10, height=2)
-button.pack(anchor=tk.W,padx=2, pady=5)
-
-window.mainloop()
+if __name__ == "__main__":
+    createGUI()
